8a856b600788377ef9e22b58c0d1355738ea4e7f
0xYoyo <yoyo2@Yoyo.localdomain>
2022-09-27 22:24:44 +0300

    deleted bell

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -95,7 +95,6 @@ export default function LotteryEntrance() {
             message: "Transaction Complete!",
             title: "Transaction Notification",
             position: "topR",
-            icon: "bell",
         })
     }
 
